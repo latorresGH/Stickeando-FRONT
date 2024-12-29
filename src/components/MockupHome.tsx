a650496d67a321417c40109113a4361b35b15848
laTorresGH <laureano26112001@hotmail.com>
2024-12-29 05:32:24 -0300

    cambios al menu realizados y mejorados

--- src/components/MockupHome.tsx
+++ src/components/MockupHome.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/MockupHome.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const MockupHome = () => {
   return (
@@ -17,6 +18,28 @@ const MockupHome = () => {
         <p className={styles.segundoTexto}>
           Encontrá tus <span className={styles.span}>stickers</span> en
         </p>
+      </div>
+
+      <div className={styles.redesContainer}>
+        <div className={styles.redesBox1}>
+ 
+          <Link
+            href="https://www.instagram.com/stic.keando/"
+            className={styles.boton}
+          >
+          <span className={styles.textoBox1}>Instagram</span>
+          </Link>
+        </div>
+
+        <div className={styles.redesBox2}>
+
+          <Link
+            href="https://wa.me/5493425824554"
+            className={styles.boton2}
+          >
+          <span className={styles.textoBox2}>Contactanos</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
