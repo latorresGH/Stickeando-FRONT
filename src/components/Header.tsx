5f6ddacf27e7040144a5626ab3df0fb002871faf
laTorresGH <laureano26112001@hotmail.com>
2024-12-28 21:33:53 -0300

    create header not logged

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,42 +2,65 @@
 
 import React from "react";
 import { useUser } from "@/context/authContext";
+import "@/styles/header.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   const { user } = useUser();
 
   return (
-    <header>
-      <h1>Mi Aplicación</h1>
-      <nav>
-        {!user ? (
-          // No autenticado
-          <button>
-            Iniciar sesión
-          </button>
-        ) : (
+    <header className="header">
+      <nav className="navHeader">
+        <ul className="menu">
+          <Image className="logo"
+            src="/images/Stickeando-icon.webp"
+            alt="Logo"
+            width={55}
+            height={55}
+          />
           <div>
-            {/* Foto y nombre */}
-            {user.foto_perfil ? (
-              <img
-                src={user.foto_perfil}
-                alt={user.nombre}
-              />
-            ) : (
-              <span>
-                {user.nombre[0].toUpperCase()}
-              </span>
-            )}
-            <span>{user.nombre}</span>
+            <div className="menuLinks">
+              <li>
+                <Link href="/productos">
+                  Productos
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacto">
+                  Contacto
+                </Link>
+              </li>
 
-            {/* Mostrar "Panel administrador" solo si es administrador */}
-            {user.rol === "administrador" && (
-              <button>
-                Panel administrador
-              </button>
-            )}
+              {!user ? (
+                // No autenticado
+                <Link 
+                className="login"
+                href="/login"
+                >
+                  Iniciar sesión
+                </Link>
+              ) : (
+                <div>
+                  {/* Foto y nombre */}
+                  {user.foto_perfil ? (
+                    <img src={user.foto_perfil} alt={user.nombre} />
+                  ) : (
+                    <span>{user.nombre[0].toUpperCase()}</span>
+                  )}
+                  <span>{user.nombre}</span>
+
+                  {/* Mostrar "Panel administrador" solo si es administrador */}
+                  {user.rol === "administrador" && (
+                    <Link href="/admin">
+                      Panel administrador
+                    </Link>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
-        )}
+        </ul>
       </nav>
     </header>
   );
