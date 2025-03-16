4321fb3cf7f31fb6357ec007d99c05e085f58ce0
laTorresGH <laureano26112001@hotmail.com>
2025-03-16 05:23:47 -0300

    cambios

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["stickeando.onrender.com"], // Permite imágenes de este dominio
+    domains: [
+      "stickeando.onrender.com",  // Si tus imágenes están alojadas en Render
+      "stickeando.vercel.app",    // Si las imágenes están alojadas en Vercel
+    ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/old-route',
+        destination: '/new-route',
+        permanent: true,
+      },
+    ]
+  },
+  reactStrictMode: true, // Es una buena práctica mantener el modo estricto activado
 };
 
 export default nextConfig;
